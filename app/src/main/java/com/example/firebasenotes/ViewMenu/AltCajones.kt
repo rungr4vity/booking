49f04b75073bf1845727c4cd3eef400fef6d961f
DESKTOP-9HM87HR\Isita <erickalonso488@gmail.com>
2024-05-08 14:54:16 -0600

    List-Cajon

--- app/src/main/java/com/example/firebasenotes/ViewMenu/AltCajones.kt
+++ app/src/main/java/com/example/firebasenotes/ViewMenu/AltCajones.kt
@@ -1,11 +1,16 @@
 package com.example.firebasenotes.ViewMenu
 
 import android.annotation.SuppressLint
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import com.example.firebasenotes.WidgetsCardView.Listing.ListingDrawer.DrawerScreen
+import com.example.firebasenotes.WidgetsCardView.Listing.ListingDrawer.Registre
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AltCajon(){
+    Scaffold {
 
-}
+    Registre()
+}}
--- app/src/main/java/com/example/firebasenotes/WidgetsCardView/Listing/ListingDrawer/DrawerScreen.kt
+++ app/src/main/java/com/example/firebasenotes/WidgetsCardView/Listing/ListingDrawer/DrawerScreen.kt
@@ -1,8 +1,11 @@
 package com.example.firebasenotes.WidgetsCardView.Listing.ListingDrawer
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,32 +16,45 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-@Preview
+import androidx.navigation.NavController
+
 @Composable
-fun DrawerScreen(drawerViewModel: DrawerViewModel = viewModel()){
+fun DrawerScreen(drawerViewModel: DrawerViewModel = viewModel(), navController: NavController){
     val cajones = drawerViewModel.stateDrawer.value
     LazyColumn {
         items(cajones){cajon->
-            ComponentDrawer(cajon = cajon)
+            ComponentDrawer(cajon = cajon, onDeleteClicked = {}, navController = navController)
         }
     }
 }
 
 @Composable
-fun ComponentDrawer(cajon : DataDrawer){
+fun ComponentDrawer(
+    cajon: DataDrawer, onDeleteClicked: () -> Unit,
+    navController: NavController
+){
+
     Card (modifier = Modifier
+        .clickable {
+            navController.navigate("Mis reservas")
+        }
         .padding(horizontal = 16.dp, vertical = 8.dp)
-        .fillMaxWidth()){
+        .fillMaxWidth(),
+    ){
         Column(modifier = Modifier
-            .padding(16.dp)
+            .padding(10.dp)
             .fillMaxWidth(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally)
         {
-            Text(text =cajon.nombre )
-            Text(text =cajon.descripcion)
+            Text(text =cajon.Nombre)
+            //Text(text =cajon.Piso)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = "Cajon: ${cajon.Piso}")
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = "Piso 12")
+            Text(text = cajon.Numero)
         }
     }
 }
