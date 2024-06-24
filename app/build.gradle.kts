90ed1dfb775e03d893bb2468c04abe10657ea6ea
DESKTOP-9HM87HR\Isita <erickalonso488@gmail.com>
2024-06-24 16:09:44 -0600

    Se resolvio el Bug de cargar imagen (Editar su informacion)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,10 @@ dependencies {
     implementation ("com.google.firebase:firebase-database-ktx:20.0.0")
     implementation("io.coil-kt:coil-compose:2.0.0")
     implementation ("androidx.compose.material:material-icons-extended:1.3.1")
+    implementation("io.coil-kt:coil-compose:1.5.0")
+
+    implementation ("com.squareup.picasso:picasso:2.8")
+
 
 
 }
